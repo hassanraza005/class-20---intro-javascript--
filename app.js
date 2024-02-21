1de60042f21f0516cf7168cf21d81e00ec54e7d0
Muhammad Hassan Raza <hassanr567890@gmail.com>
2024-02-21 10:31:08 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+// var firstName = "Muhammad"
+// var lastName = "Hassan"
+// var age = 20;
+// var isUser = false
+
+// console.log("my name is " + firstName  + " " + lastName + " and my age is " + age );
+
+// var firstName = "Muhammad"
+// alert("hello how are you?" + firstName);
+
+var firstName = "Hassan"
+var lastName = "Raza"
+var age = "22"
+var isUser = true
+var eduction = "Matric"
+var city = "karachi"
+var skill = "backEndDeveloper"
+var vehiche = "bike"
+var countryName = "pakistan"
+var postelcode = "0005"
+
+console.log ("my name is " + firstName  + " " + lastName + " and my age is " + age + ". your user:" + isUser);
+
+console.log(typeof(isUser))
+console.log(isUser)
+console.log(
+    eduction
+)
+console.log(city)
+console.log(skill)
+console.log(vehiche)
+console.log(countryName)
+console.log(postelcode)
+
+console
